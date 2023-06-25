e097fc4e200951331e1f1675de153cb5ba7eb31e
Bruno Marques <brunolmarques@gmail.com>
2023-06-24 23:05:50 -0700

    .gitignore added

--- target/debug/deps/demonr_cli-016e61a3e2dd3bae.d
+++
@@ -1,5 +0,0 @@
-/Users/bruno/Programming/demonr-cli/target/debug/deps/demonr_cli-016e61a3e2dd3bae.rmeta: src/main.rs
-
-/Users/bruno/Programming/demonr-cli/target/debug/deps/demonr_cli-016e61a3e2dd3bae.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/demonr_cli-b016efc217cc3002.d
+++
@@ -1,5 +0,0 @@
-/Users/bruno/Programming/demonr-cli/target/debug/deps/demonr_cli-b016efc217cc3002.rmeta: src/main.rs
-
-/Users/bruno/Programming/demonr-cli/target/debug/deps/demonr_cli-b016efc217cc3002.d: src/main.rs
-
-src/main.rs:
